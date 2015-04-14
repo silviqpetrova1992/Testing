77a04133da86f01455f0ed819719aadc53f94ae9
silviqpetrova1992 <silviqpetrova1992@gmail.com>
2015-04-14 14:44:09 +0300

    Add @BeforeClass reduce the duplications.

--- test/task1/SumatorTest.java
+++ test/task1/SumatorTest.java
@@ -1,6 +1,6 @@
 package task1;
 
-import junit.framework.TestCase;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import static org.hamcrest.core.Is.is;
@@ -10,19 +10,20 @@ import static org.junit.Assert.assertThat;
  * Created by Silvia Petrova(silviqpetrova1992@gmail.com)on 4/14/15.
  */
 public class SumatorTest{
+  private static Sumator sumator;
+  @BeforeClass public  static void initialize(){
+    sumator=new Sumator();
+  }
   @Test
   public void happyPath() {
-     Sumator sumator=new Sumator();
     assertThat(sumator.sum("5", "4"), is("9"));
   }
   @Test(expected = NumberFormatException.class)
   public void illigalData() {
-     Sumator sumator=new Sumator();
    sumator.sum("erwer","8");
   }
   @Test(expected = IllegalArgumentException.class)
   public void nullArgument() {
-    Sumator sumator=new Sumator();
     sumator.sum(null,"8");
 
   }
